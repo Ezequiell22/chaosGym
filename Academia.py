3c1869c6fa0ff45560c1f0bf30907090e0268c22
ezequiel <contatodevmais@gmail.com>
2024-12-16 16:40:59 -0300

    init

---
+++ Academia.py
@@ -0,0 +1,60 @@
+import random
+
+class Academia:
+  def __init__(self):
+      self.halteres = [i for i in range( 10, 50) if i % 2==0]
+      self.porta_halteres = {}
+      self.reiniciar_o_dia()
+  
+  def reiniciar_o_dia(self):
+     self.porta_halteres = {i:i for i in self.halteres}
+     print(f'lista halteres {self.porta_halteres}')
+  
+  def listar_halteres(self):
+     lista = [i for i in self.porta_halteres.values() if i != 0]
+     print(f'listar_halteres {lista}')
+     return lista
+   
+  def listar_espacos(self):
+     return [i for i, j in self.porta_halteres.items() if j == 0]
+
+  def pegar_haltere(self, peso):
+    halt_pos = list(self.porta_halteres.values()).index(peso)
+    key_halt = list(self.porta_halteres.keys())[halt_pos]
+    self.porta_halteres[key_halt] = 0
+    return peso
+  
+  def devolver_haltere(self, Key, peso):
+     if self.porta_halteres[Key] != 0:
+         raise ValueError(f'Essa posição ja está ocupada {self.porta_halteres[Key]}')
+     
+     self.porta_halteres[Key] = peso
+  
+  def calcular_caos(self):
+     num_caos = [i for i, j in self.porta_halteres.items() if i != j]
+     return len(num_caos) / len(self.porta_halteres)
+  
+
+class Usuario:
+   def __init__(self, tipo, academia : Academia):
+      self.tipo = tipo # 1 normal 2 bagunceiro
+      self.academia = academia
+      self.peso = 0
+   def iniciar_execicio(self):
+      lista_pesos = self.academia.listar_halteres()
+      self.peso = random.choice(lista_pesos)
+      self.academia.pegar_haltere(self.peso) 
+   def finalizar_exercicio(self):
+      espacos = self.academia.listar_espacos()
+      print(f'espaços {espacos}')
+
+      if self.tipo == 1:
+         if self.peso in espacos:
+            self.academia.devolver_haltere(self.peso, self.peso)
+         else:
+            pos = random.choice(espacos)
+            self.academia.devolver_haltere(pos, self.peso)
+      
+      if self.tipo == 2:
+         pos = random.choice(espacos)
+         self.academia.devolver_haltere(pos, self.peso)
---
+++ main.py
@@ -0,0 +1,30 @@
+from Academia import Academia, Usuario
+import random
+
+academia = Academia()
+
+usuarios = [ Usuario(1, academia )  for i in range(10)]
+usuarios += [ Usuario(2, academia )  for i in range(2)]
+random.shuffle(usuarios)
+
+list_chaos = []
+
+def rotina():
+  for i in range(10):
+    random.shuffle(usuarios)
+
+    for user in usuarios:
+      user.iniciar_execicio()
+    
+    for user in usuarios:
+      user.finalizar_exercicio()
+
+#testes 20
+for k in range(20):
+  print(f'Teste {k}')
+  academia.reiniciar_o_dia()
+  rotina()
+  list_chaos += [academia.calcular_caos() * 100]
+
+print(f'{list_chaos} %')
+
